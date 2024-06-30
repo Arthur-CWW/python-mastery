f5cb67bc7ff703505b5f299c90f256391dd075cd
chun1 <arthur@chunai.dev>
2024-06-30 16:54:00 +1000

    ex 1.2

---
+++ src/pcost.py
@@ -0,0 +1,14 @@
+with open("Data/portfolio.dat", "r") as file:
+    total = 0
+    for line in file:
+        print(line)
+        words = line.strip().split(" ")
+        print(words)
+        name, quantity, price = words
+        # for quantity, price in words:
+        # print(quantity, price)
+        try:
+            total += int(quantity) * float(price)
+        except ValueError:
+            print(f"Error:{name} {quantity} or {price} is not a number")
+    print(total)
